5c77b2e7fb5b47bf9c50e0d90c543a3436fe9f26
svshriyansh <prince.vishwakarma2000@gmail.com>
2022-05-24 22:27:07 +0530

    authO testing

---
+++ auth.js
@@ -0,0 +1,25 @@
+const passport = require("passport");
+var GoogleStrategy = require("passport-google-oauth2").Strategy;
+GOOGLE_CLIENT_ID =
+  "165328502385-kjai1tn72alrthpp402o1cm5amhissg6.apps.googleusercontent.com";
+GOOGLE_CLIENT_SECRET = "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";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:5000/google/callback",
+      passReqToCallback: true,
+    },
+    function (request, accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const passport = require("passport");
+const session = require("express-session");
+require("./auth");
+const app = express();
+
+function isLoggedIn(req, res, next) {
+  console.log(req.user);
+  req.user ? next() : res.sendStatus(401);
+}
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", (req, res) => {
+  res.send('<a href="/auth/google">Authenticating with google</a>');
+});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+app.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/protected",
+    failureRedirect: "/auth/failure",
+  })
+);
+
+app.get("/protected", isLoggedIn, (req, res) => {
+  res.send(`Hello ${req.user.displayName}`);
+});
+app.get("/logout", (req, res, next) => {
+  req.logout();
+  req.session.destroy();
+  res.send("Goodbuy...!");
+});
+app.get("/auth/failure", (req, res) => {
+  res.send("something went wrong");
+});
+app.listen(5000, () => console.log("Listing on 5000"));
