5356803b0a7de3f1605740b7a994d0a708a1623b
svshriyansh <prince.vishwakarma2000@gmail.com>
2022-05-25 23:02:13 +0530

    Url changed

--- auth.js
+++ auth.js
@@ -10,8 +10,8 @@ passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/google/callback",
-      // callbackURL: "https://demo-autho.herokuapp.com/google/callback",
+      // callbackURL: "http://localhost:5000/google/callback",
+      callbackURL: "https://demo-autho.herokuapp.com/google/callback",
       passReqToCallback: true,
     },
     function (request, accessToken, refreshToken, profile, done) {
