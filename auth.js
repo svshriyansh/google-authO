9f66c23f6020fc637ec92964ed8be21781b05c17
svshriyansh <prince.vishwakarma2000@gmail.com>
2022-05-25 22:57:06 +0530

    UI added

--- auth.js
+++ auth.js
@@ -5,14 +5,13 @@ const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
 // GOOGLE_CLIENT_ID =
 //   "165328502385-kjai1tn72alrthpp402o1cm5amhissg6.apps.googleusercontent.com";
 // GOOGLE_CLIENT_SECRET = "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";
-
 passport.use(
   new GoogleStrategy(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      // callbackURL: "http://localhost:5000/google/callback",
-      callbackURL: "https://demo-autho.herokuapp.com/google/callback",
+      callbackURL: "http://localhost:5000/google/callback",
+      // callbackURL: "https://demo-autho.herokuapp.com/google/callback",
       passReqToCallback: true,
     },
     function (request, accessToken, refreshToken, profile, done) {
